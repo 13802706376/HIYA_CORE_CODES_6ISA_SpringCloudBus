32f093b153df53e2a0aa767cdd97202b452dfc02
caozhijun <caozhijun@caozhijun-book>
2018-11-27 20:14:01 +0800

    https://github.com/13802706376/SpringCloudBus.git

---
+++ src/main/java/com/hiya/cloud/Receiver.java
@@ -0,0 +1,20 @@
+package com.hiya.cloud;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(queues = "hello")
+public class Receiver
+{
+    private static final Logger log = LoggerFactory.getLogger(Receiver.class);
+
+    @RabbitHandler
+    public void process(String hello)
+    {
+        log.info("Receiver : " + hello);
+    }
+}
